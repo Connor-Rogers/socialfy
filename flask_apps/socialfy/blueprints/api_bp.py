d449b82f12ce54d34ddab63d1e7c67771c8f1de1
rogersc7atwit <connorrog01@icloud.com>
2023-03-01 20:26:01 -0500

    Init Repo. -Setup Initial Structure -Outlined Methods for Library Objects -Resarched and Stored Method for Oauth in auth_bp.py

---
+++ flask_apps/socialfy/blueprints/api_bp.py
@@ -0,0 +1,9 @@
+from flask import blueprints
+#Social API Blueprint 
+
+#TODO:Like/Unlike Endpoint 
+
+#TODO:Generate Feed Endpoint 
+
+#TODO: Comment on a Post Endpoint 
+
---
+++ flask_apps/socialfy/lib/feed.py
@@ -0,0 +1,7 @@
+#generate user feed
+class feed:
+    pass
+def song_post():
+    pass
+def playlist_post():
+    pass
